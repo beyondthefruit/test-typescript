e51095069059f76edf4a7041d9a2dfdeff262b99
Kevin Hanard <kevin.hanard@gmail.com>
2022-11-25 11:30:20 +1100

    practice ts

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,23 @@
 var hello = 'world';
 hello = 'foo';
+var getFullName = function (name, surname) {
+    return name + '' + surname;
+};
+// console.log(getFullName('moster', 1));
+//this is false as 1 isn't a string
+console.log(getFullName('moster', 'blabla'));
+//object
+var user = {
+    name: 'Poilu',
+    age: 30,
+    getMessage: function () {
+        return 'Hey' + name;
+    }
+};
+var user2 = {
+    name: 'Jack',
+    getMessage: function () {
+        return 'Hey' + name;
+    }
+};
+console.log(user.getMessage);
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,32 @@
-let hello = 'world';
+let hello: string = 'world';
 hello = 'foo';
+
+const getFullName = (name: string, surname: string): string => {
+  return name + '' + surname;
+};
+
+// console.log(getFullName('moster', 1));
+//this is false as 1 isn't a string
+console.log(getFullName('moster', 'blabla'));
+
+interface IUser {
+  name: string;
+  age?: number; //means that it's not mandatory
+  getMessage(): string;
+}
+//object
+const user: IUser = {
+  name: 'Poilu',
+  age: 30,
+  getMessage() {
+    return 'Hey' + name;
+  },
+};
+const user2: IUser = {
+  name: 'Jack',
+  getMessage() {
+    return 'Hey' + name;
+  },
+};
+
+console.log(user.getMessage);
